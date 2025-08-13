a5d9f7c6a34cc4d6e2d7c58f189b4ed21d62beb4
Fekri Saleh <fekri600@gmail.com>
2025-08-12 19:14:46 -0600

    deploy:rigger deployment

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = ""
+    key            = "envs/terraform.tfstate"
+    region         = ""
+    dynamodb_table = ""
+    encrypt        = true
+  }
+}
--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.11.4"
+  required_version = "v1.12.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.97.0"
+      version = "6.7.0"
     }
   }
 
