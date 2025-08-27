13fe3ddad799d454613489d25d93e93b484a5e4d
Fekri Saleh <fekri600@gmail.com>
2025-08-27 13:04:05 -0600

    fix ssm bugs

--- data.tf
+++ data.tf
@@ -1,19 +1,23 @@
-data "ssm_parameter" "artifacts_bucket_arn" {
+data "aws_ssm_parameter" "artifacts_bucket_arn" {
   name = "/i2508dr/ci/artifacts_bucket_arn"
 }
-data "ssm_parameter" "rds_snapshot_s3_key" {
+
+data "aws_ssm_parameter" "rds_snapshot_s3_key" {
   name = "/i2508dr/ci/rds_snapshot_s3_key"
 }
-data "ssm_parameter" "rds_failover_s3_key" {
+
+data "aws_ssm_parameter" "rds_failover_s3_key" {
   name = "/i2508dr/ci/rds_failover_s3_key"
 }
-data "ssm_parameter" "rds_snapshot_code_hash" {
+
+data "aws_ssm_parameter" "rds_snapshot_code_hash" {
   name = "/i2508dr/ci/rds_snapshot_code_hash"
 }
-data "ssm_parameter" "rds_failover_code_hash" {
+
+data "aws_ssm_parameter" "rds_failover_code_hash" {
   name = "/i2508dr/ci/rds_failover_code_hash"
 }
 
-data "ssm_parameter" "artifacts_bucket_name" {
+data "aws_ssm_parameter" "artifacts_bucket_name" {
   name = "/i2508dr/ci/artifacts_bucket_name"
 }
