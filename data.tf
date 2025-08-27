e429cfb48b594aa007866c19b897f9cc93821479
Fekri Saleh <fekri600@gmail.com>
2025-08-27 12:45:27 -0600

    add data

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "ssm_parameter" "artifacts_bucket_arn" {
+  name = "/i2508dr/ci/artifacts_bucket_arn"
+}
+data "ssm_parameter" "rds_snapshot_s3_key" {
+  name = "/i2508dr/ci/rds_snapshot_s3_key"
+}
+data "ssm_parameter" "rds_failover_s3_key" {
+  name = "/i2508dr/ci/rds_failover_s3_key"
+}
+data "ssm_parameter" "rds_snapshot_code_hash" {
+  name = "/i2508dr/ci/rds_snapshot_code_hash"
+}
+data "ssm_parameter" "rds_failover_code_hash" {
+  name = "/i2508dr/ci/rds_failover_code_hash"
+}
+
+data "ssm_parameter" "artifacts_bucket_name" {
+  name = "/i2508dr/ci/artifacts_bucket_name"
+}
