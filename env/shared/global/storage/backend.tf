c59fce9c135aba56e1b03d4cc7306a81e0537ffe
Fekri Saleh <fekri600@gmail.com>
2025-09-24 19:33:10 -0600

    trigger: pipeline

--- env/shared/global/storage/backend.tf
+++ env/shared/global/storage/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-ci-least-privilege-automation-state-0534b6fb"
+    bucket         = "aws-ci-least-privilege-automation-state-97e54587"
     key            = "envs/storage/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
