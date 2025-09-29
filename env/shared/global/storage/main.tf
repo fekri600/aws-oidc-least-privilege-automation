9b79e13d68d3018ad9cec43cc320627e3bf311b7
Fekri Saleh <fekri600@gmail.com>
2025-09-28 20:41:09 -0600

    update-1759113669

--- env/shared/global/storage/main.tf
+++ env/shared/global/storage/main.tf
@@ -1,8 +1,11 @@
 
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
 
 module "artifacts_bucket" {
   source            = "../../../../modules/s3"
-  name              = "${var.name_prefix}-shared-bkt"
+  name              = "${var.name_prefix}-shared-bkt-${random_id.bucket_suffix.hex}"
   enable_versioning = false
   force_destroy     = true
   kms_key_arn       = null # or leave null for AES256
