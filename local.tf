20922990c9efbeaec195605a3ca57fe9ddd9818f
Fekri Saleh <fekri600@gmail.com>
2025-09-28 19:23:15 -0600

    update-1759108995

--- local.tf
+++ local.tf
@@ -1,7 +1,7 @@
 locals {
   prefix = {
-    prod_us_east_1 = "i2509demo_prod_us_east_1"
-    prod_us_west_1 = "i2509demo_prod_us_west_1"
-    shared = "i2509demo_shared"
+    prod_us_east_1 = "i2509demo-prod-us-east-1"
+    prod_us_west_1 = "i2509demo-prod-us-west-1"
+    shared = "i2509demo-shared"
   }
 }   
