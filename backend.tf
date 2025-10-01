b0aa212758e10b82bb16ae7f198652f130783edd
Fekri Saleh <fekri600@gmail.com>
2025-10-01 12:36:22 -0600

    update-1759343782

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aws-ci-least-privilege-automation-state-b33caaa3"
+    bucket         = "aws-oidc-least-privilege-automation-state-ace3823d"
     key            = "envs/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true
